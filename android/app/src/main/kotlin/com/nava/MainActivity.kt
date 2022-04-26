571e05be07a7317d2e08b6f24e48f8a535650df6
FlutLab.io <>
2022-04-26 16:17:07 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/nava/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nava
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Practica Drawer';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        actions: [
+          Icon(Icons.search),
+          Icon(Icons.more_vert)
+        ],
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('assets/descarga.png'),
+          Image.asset('assets/descarga.png'),
+          Image.asset('assets/descarga.png'),
+          //Image.asset('images/pic2.jpg'),
+          //Image.asset('images/pic3.jpg'),
+        ],
+      ),
+      drawer: Drawer(
+        // Agrega un ListView al drawer. Esto asegura que el usuario pueda desplazarse
+        // a través de las opciones en el Drawer si no hay suficiente espacio vertical
+        // para adaptarse a todo.
+        child: ListView(
+          // Importante: elimina cualquier padding del ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Encabezado de Drawer'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('alumno 1'),
+              onTap: () {
+                // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('alumno 2'),
+              onTap: () {
+                // // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('alumno 3'),
+              onTap: () {
+                // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
